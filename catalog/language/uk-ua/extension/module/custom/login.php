3a99a062dfbadfdd4c2f20b652def3a115fcb71c
Sergey <cergey.chernyavskiy.96@gmail.com>
2025-04-02 15:19:17 +0300

    lesson31

---
+++ catalog/language/uk-ua/extension/module/custom/login.php
@@ -0,0 +1,12 @@
+<?php
+
+$_['heading_login'] = 'Оформити замовлення як';
+
+$_['text_auth'] = 'Вже зареєстрований';
+$_['text_register'] = 'Зареєструватися';
+$_['text_guest'] = 'Гість';
+$_['text_forgotten'] = 'Забули пароль?';
+$_['text_loading'] = 'Завантаження';
+$_['entry_email'] = 'E-mail';
+$_['entry_password'] = 'Пароль';
+$_['button_login'] = 'Увійти';
